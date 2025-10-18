148039d4ab5c508ecc19005b3bcbc2dcf7c0337e
ranalatifwaqas408-byte <ranalatifwaqas408@gmail.com>
2025-10-18 15:32:31 +0500

    commit
    print the larger number

---
+++ practice/main.py
@@ -0,0 +1,8 @@
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+if(n1>n2):
+    print("the larger number is n1",n1)
+elif(n2>n1):
+    print("the larger umber is n2",n2)
+else:
+    print("Both numbers are equal")
