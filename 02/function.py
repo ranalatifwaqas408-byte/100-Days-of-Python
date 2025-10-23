3328cdb30133963a2346eed49c7d347b0e812744
ranalatifwaqas408-byte <ranalatifwaqas408@gmail.com>
2025-10-24 00:19:41 +0500

    command

---
+++ 02/function.py
@@ -0,0 +1,31 @@
+def calculateGmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+
+a=7
+b=8
+if(a>b):
+    print("First number is greater")
+else:
+    print("Second number is greater")
+calculateGmean(a,b)
+# gmean1=(a*b)/(a+b)
+# print(gmean1)
+c=8
+d=74
+if(c>d):
+    print("First number is greater")
+else:
+    print("Second number is greater")
+calculateGmean(c,d)
+# gmean2=(c*d)/(c+b)
+# print(gmean2)
+
+    
+
+
+
+
+
+
+
