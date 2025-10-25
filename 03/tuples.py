d710f13c3f99726c832449cc2eb30ad6895a986d
ranalatifwaqas408-byte <ranalatifwaqas408@gmail.com>
2025-10-25 15:42:43 +0500

    commit

---
+++ 03/tuples.py
@@ -0,0 +1,12 @@
+tup=(1,2,4,6,8,"green")
+print(type(tup),tup)
+print(tup[0])
+print(tup[5])
+print(len(tup))
+if 4 in tup:
+    print("Yes it is present")
+
+animals=("cat","dog","bat","mouse","pig","horse","donkey","cow")
+print(animals[1:7:3])
+
+                                #    tuples methods
