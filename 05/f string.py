e2ff735bf43ba745c322b3b6b9c435935ebe7c0d
ranalatifwaqas408-byte <ranalatifwaqas408@gmail.com>
2025-10-25 17:13:12 +0500

    operate

---
+++ 05/f string.py
@@ -0,0 +1,11 @@
+letter="hey my name is {} and I am from {}"
+country="Pakistan"
+name="Latif"
+print(letter.format(name,country))
+print(f"hey my name is {{name}} and I am from {{country}}")
+print(f"{2*30}")
+
+
+price=49.09999
+txt=f"for only {price:.2f} dollars!"
+print(txt)
