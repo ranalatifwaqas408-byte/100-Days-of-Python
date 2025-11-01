5c9473509714a49fc2fcc349876e02aa4bc0f76d
ranalatifwaqas408-byte <ranalatifwaqas408@gmail.com>
2025-11-01 13:38:24 +0500

    commit

---
+++ 09/dictionary.py
@@ -0,0 +1,18 @@
+ep1={122:45,123:89,567:69,670:69}
+ep2={222:67,566:90}
+ep1.clear()
+print(ep1)                              #here clear metthod removes key values from ep1
+ep1.update(ep2)
+print(ep1)                              #here update method adds key value pairs of ep2 in ep1
+
+
+
+
+
+employees={101:"Ali",102:"Sara",103:"Omar",104:"Zara"}
+employees.pop(101)                                       #here pop method removeskey value pair of key 101
+print(employees)
+
+
+
+
---
+++ 09/forloop.py
@@ -0,0 +1,28 @@
+# for i in[]:
+#     print(i)
+# else:
+#     print("Sorry no i ")
+
+
+# for i in range(6):
+#     print(i)
+#     if i==4:
+#         break
+#     else:
+#         print("not yet")
+
+
+# i=0
+# while i<7:
+#  print(i)
+# if(i==4):
+#  breakpoint
+
+
+
+
+for x in range(5):
+    print("iteration no{} in for loop".format(x+1))
+else:
+    print("else block in loop")
+    print("out of loop")
