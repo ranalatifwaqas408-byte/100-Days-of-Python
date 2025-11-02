12c5a9501f41c95e4b667d4f0f60d2eae90f20d2
ranalatifwaqas408-byte <ranalatifwaqas408@gmail.com>
2025-11-02 16:37:58 +0500

    commit

---
+++ 08/error_handling.py
@@ -0,0 +1,12 @@
+a=input("Enter a number:")
+print(f"Multiplicationn table of {a} is:")
+
+for i in range(1,11):
+    print(f"{a} X {i}= {a*i}") 
+print("invalid input")
+
+
+print("Some lines of code")
+print("End of program")
+
+
