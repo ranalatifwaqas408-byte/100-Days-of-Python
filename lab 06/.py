19f03974b1f020df444e1ae98dd0e27ba13afd7c
ranalatifwaqas408-byte <ranalatifwaqas408@gmail.com>
2025-11-03 15:36:23 +0500

    commit

---
+++ lab 06/.py
@@ -0,0 +1,10 @@
+# Question 1
+a=float(input("Enter first number:"))
+b=float(input("Enter second number:"))
+def calculator(a,b):
+    addition=a+b
+    subtraction=a-b
+    return addition,subtraction
+print("Addition and Subtraction are:",calculator(a,b))
+
+# Question 2
