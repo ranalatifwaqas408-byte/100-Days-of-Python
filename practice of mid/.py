12adce5086faffb05fe5858f1e2a5a3fb622bb77
ranalatifwaqas408-byte <ranalatifwaqas408@gmail.com>
2025-11-22 01:34:24 +0500

    command

---
+++ practice of mid/.py
@@ -0,0 +1,50 @@
+# print("Let's code")
+# # OR
+# print('Let\'s code')
+# x = 10
+# def show():
+#     global x
+#     print(x)
+#     x = 20
+# show()
+# n = int(input("Enter n: "))
+# total = 0
+# for i in range(1, n + 1):
+#     total += i
+# print("Sum =", total)s = input("Enter string: ")
+
+
+
+# s = input("Enter string: ")
+# print("Reversed:", s[::-1])
+
+# num = int(input("Enter an integer: "))
+# count = 0
+
+# while num != 0:
+#     count += 1
+#     num //= 10   # removes the last digit
+
+# print("Number of digits:", count)
+
+
+
+# def count_case(s):
+#     upper = 0
+#     lower = 0
+#     for ch in s:
+#         if ch.isupper():
+#             upper += 1
+#         elif ch.islower():
+#             lower += 1
+#     return upper, lower
+
+# # main
+# text = input("Enter a string: ")
+# u, l = count_case(text)
+# print("Uppercase letters:", u)
+# print("Lowercase letters:", l)def reverse_num(n):
+
+
+
+
--- test program/palindrome.py
+++ test program/palindrome.py
@@ -3,3 +3,16 @@ if num==num[::-1]:
     print("The number is palindrome.")
 else:
     print("The number is not palindrome.")
+
+    # Function to check if a string is palindrome
+
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")  # ignore spaces and case
+    return s == s[::-1]             # reverse check
+
+# Example
+word = input("Enter a word: ")
+if is_palindrome(word):
+    print("It's a palindrome!")
+else:
+    print("Not a palindrome.")
