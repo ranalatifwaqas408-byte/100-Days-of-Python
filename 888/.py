ca7ef711ce56da46b57dee926fc07ccc7f2142b6
ranalatifwaqas408-byte <ranalatifwaqas408@gmail.com>
2025-12-28 15:45:26 +0500

    amit

--- 888/.py
+++ 888/.py
@@ -32,3 +32,17 @@ while n > 0:
     n //= 10
 
 print("Reverse =", rev)
+
+
+n = int(input("Enter terms: "))
+a = 0
+b = 1
+
+print(a)
+print(b)
+
+for i in range(2, n):
+    c = a + b
+    print(c)
+    a = b
+    b = c
