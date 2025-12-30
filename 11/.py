921df620b98313f51d08b74e1f9a418b1e1946ab
ranalatifwaqas408-byte <ranalatifwaqas408@gmail.com>
2025-12-30 17:58:37 +0500

    commit

---
+++ 11/.py
@@ -0,0 +1,23 @@
+a=330
+b=3303
+print("A") if a>b else print("=") if a==b else print("B")
+c=9 if a>b else 0
+print(c)
+result="value_if_true" if a>b else "value_if_false"
+
+marks=[12,56,32,98,12,45,1,4]
+index = 0
+for index,mark in enumerate(marks):
+ print(marks)
+if(index==3):
+    print("Harry,awesome!")
+    
+
+fruits=["apple","banana","cherry"]
+for index,fruit in enumerate(fruits):
+   print(index,fruit)
+
+colours=["red","green","blue","yellow"]
+for index,colour in enumerate(colours):
+   print(index,colour)
+    
